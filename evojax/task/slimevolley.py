f3e233ee38892a0f1b9e9dec1ebfa24e6db05a02
Anthony <26030673+im-ant@users.noreply.github.com>
2025-02-16 21:56:13 -0500

    bug fixes for rendering

--- evojax/task/slimevolley.py
+++ evojax/task/slimevolley.py
@@ -335,8 +335,9 @@ class Particle:
         self.c = c
 
     def display(self, canvas):
-        return circle(canvas, toX(float(self.p.x)), toY(float(self.p.y)),
-                      toP(float(self.p.r)), color=self.c)
+        _px, _py, _pr = self.p.x.item(), self.p.y.item(), self.p.r.item()
+        return circle(canvas, toX(float(_px)), toY(float(_py)),
+                      toP(float(_pr)), color=self.c)
 
     def move(self):
         self.p = ParticleState(self.p.x+self.p.vx*TIMESTEP,
@@ -591,13 +592,13 @@ class Agent:
         return getObsArray(self.state)
 
     def display(self, canvas, ball_x, ball_y):
-        bx = float(ball_x)
-        by = float(ball_y)
+        bx = float(ball_x.item())
+        by = float(ball_y.item())
         p = self.p
-        x = float(p.x)
-        y = float(p.y)
-        r = float(p.r)
-        direction = int(p.direction)
+        x = float(p.x.item())
+        y = float(p.y.item())
+        r = float(p.r.item())
+        direction = int(p.direction.item())
 
         angle = math.pi * 60 / 180
         if direction == 1:
@@ -624,7 +625,7 @@ class Agent:
                         color=(0, 0, 0))
 
         # draw coins (lives) left
-        num_lives = int(p.life)
+        num_lives = int(p.life.item())
         for i in range(1, num_lives):
             canvas = circle(canvas, toX(direction*(REF_W/2+0.5-i*2.)),
                             WINDOW_HEIGHT-toY(1.5), toP(0.5),
